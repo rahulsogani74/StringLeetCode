5212331551492f505e77a6db1b6525b23ca3724f
rahulsogani74 <131875485+rahulsogani74@users.noreply.github.com>
2023-07-12 00:31:31 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String a = "My Name is Ram";
+        String[] b = a.split(" ");
+
+        for (int i = b.length - 1; i >= 0; i--) {
+            System.out.print(b[i] + " ");
+        }
+    }
+}
